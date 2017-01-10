72bc8a732af9893adcf8fe8a70802b4d4112ca04
Jim Apple <github-public@jbapple.com>
2017-01-09 19:30:06 -0800

    Add C++11 support

--- suffix128.hpp
+++ suffix128.hpp
@@ -1,52 +1,59 @@
+#pragma once
+
+namespace suffix128 {
+
 constexpr unsigned __int128 CharValue(char c) {
-  if (c >= '0' && c <= '9') {
-    return c - '0';
-  } else if (c >= 'a' && c <= 'f') {
-    return 10 + (c - 'a');
-  }
-  // c >= 'A' && c <= 'F'
-  return 10 + (c - 'A');
+  return (c >= '0' && c <= '9')
+             ? (c - '0')
+             : ((c >= 'a' && c <= 'f') ? (10 + (c - 'a')) : (10 + (c - 'A')));
 }
 
-template<int BASE>
+template <int BASE>
 constexpr bool ValidateChar(char c) {
-  if (BASE <= 10) {
-    return c >= '0' && c <= ('0' + (BASE - 1));
-  }
-  return ((c >= '0') && (c <= '9')) || ((c >= 'a') && (c <= 'f')) ||
-         ((c >= 'A') && (c <= 'F'));
+  return (BASE <= 10)
+             ? (c >= '0' && c <= ('0' + (BASE - 1)))
+             : (((c >= '0') && (c <= '9')) || ((c >= 'a') && (c <= 'f')) ||
+                ((c >= 'A') && (c <= 'F')));
 }
 
-template<int BASE, char... CS>
-constexpr bool ValidateU128() {
-  char cs[] = {CS...};
-  unsigned __int128 accumulate = 0;
-  for (decltype(sizeof...(CS)) i = 0; i < sizeof...(CS); ++i) {
-    if (cs[i] == '\'') continue;
-    if (!ValidateChar<BASE>(cs[i])) return false;
-    constexpr unsigned __int128 MAX128 = ~static_cast<unsigned __int128>(0);
-    if (accumulate > MAX128 / BASE) return false;
-    accumulate *= BASE;
-    unsigned __int128 cval = CharValue(cs[i]);
-    if (MAX128 - cval < accumulate) return false;
-    accumulate += cval;
-  }
+static constexpr unsigned __int128 MAX128 = ~static_cast<unsigned __int128>(0);
+
+template <int BASE>
+constexpr bool ValidateU128Helper(unsigned __int128) {
   return true;
+}
+
+template <int BASE, char C, char... CS>
+constexpr bool ValidateU128Helper(unsigned __int128 accumulate) {
+  return (C == '\'') ? ValidateU128Helper<BASE, CS...>(accumulate)
+                     : ((accumulate <= MAX128 / BASE) &&
+                        (BASE * accumulate <= MAX128 - CharValue(C)) &&
+                        ValidateU128Helper<BASE, CS...>(accumulate * BASE +
+                                                        CharValue(C)));
+}
+
+template <int BASE, char... CS>
+constexpr bool ValidateU128() {
+  return ValidateU128Helper<BASE, CS...>(0);
+}
+
+template <int BASE>
+constexpr unsigned __int128 MakeU128Helper(unsigned __int128 result) {
+  return result;
+}
+
+template <int BASE, char C, char... CS>
+constexpr unsigned __int128 MakeU128Helper(unsigned __int128 result) {
+  return MakeU128Helper<BASE, CS...>(
+      (C == '\'') ? result : (result * BASE + CharValue(C)));
 }
 
 template <int BASE, char... CS>
 constexpr unsigned __int128 MakeU128() {
-  char cs[] = {CS...};
-  unsigned __int128 result = 0;
-  for (decltype(sizeof...(CS)) i = 0; i < sizeof...(CS); ++i) {
-    if (cs[i] == '\'') continue;
-    result *= BASE;
-    result += CharValue(cs[i]);
-  }
-  return result;
+  return MakeU128Helper<BASE, CS...>(0);
 }
 
-template<char... CS>
+template <char... CS>
 struct StaticU128 {
   static constexpr bool IS_VALID = ValidateU128<10, CS...>();
   static constexpr unsigned __int128 PAYLOAD = MakeU128<10, CS...>();
@@ -82,17 +89,13 @@ struct StaticU128<'0', CS...> {
   static constexpr unsigned __int128 PAYLOAD = MakeU128<8, CS...>();
 };
 
-template <char... CS>
-constexpr unsigned __int128 operator""_u128() {
-  static_assert(StaticU128<CS...>::IS_VALID,
-                "Invalid characters or number too large");
-  return StaticU128<CS...>::PAYLOAD;
-}
+static constexpr unsigned __int128 SIGNED_LIMIT =
+    (static_cast<unsigned __int128>(1) << 127) - 1;
 
 template <char... CS>
 struct Static128 {
   static constexpr bool IS_VALID =
-      ValidateU128<10, CS...>() && (MakeU128<10, CS...>() < (1_u128 << 127));
+      ValidateU128<10, CS...>() && (MakeU128<10, CS...>() <= SIGNED_LIMIT);
   static constexpr __int128 PAYLOAD =
       static_cast<__int128>(MakeU128<10, CS...>());
 };
@@ -100,7 +103,7 @@ struct Static128 {
 template <char... CS>
 struct Static128<'0', 'x', CS...> {
   static constexpr bool IS_VALID =
-      ValidateU128<16, CS...>() && (MakeU128<16, CS...>() < (1_u128 << 127));
+      ValidateU128<16, CS...>() && (MakeU128<16, CS...>() <= SIGNED_LIMIT);
   static constexpr __int128 PAYLOAD =
       static_cast<__int128>(MakeU128<16, CS...>());
 };
@@ -108,7 +111,7 @@ struct Static128<'0', 'x', CS...> {
 template <char... CS>
 struct Static128<'0', 'X', CS...> {
   static constexpr bool IS_VALID =
-      ValidateU128<16, CS...>() && (MakeU128<16, CS...>() < (1_u128 << 127));
+      ValidateU128<16, CS...>() && (MakeU128<16, CS...>() <= SIGNED_LIMIT);
   static constexpr __int128 PAYLOAD =
       static_cast<__int128>(MakeU128<16, CS...>());
 };
@@ -116,7 +119,7 @@ struct Static128<'0', 'X', CS...> {
 template <char... CS>
 struct Static128<'0', 'b', CS...> {
   static constexpr bool IS_VALID =
-      ValidateU128<2, CS...>() && (MakeU128<2, CS...>() < (1_u128 << 127));
+      ValidateU128<2, CS...>() && (MakeU128<2, CS...>() <= SIGNED_LIMIT);
   static constexpr __int128 PAYLOAD =
       static_cast<__int128>(MakeU128<2, CS...>());
 };
@@ -124,7 +127,7 @@ struct Static128<'0', 'b', CS...> {
 template <char... CS>
 struct Static128<'0', 'B', CS...> {
   static constexpr bool IS_VALID =
-      ValidateU128<2, CS...>() && (MakeU128<2, CS...>() < (1_u128 << 127));
+      ValidateU128<2, CS...>() && (MakeU128<2, CS...>() <= SIGNED_LIMIT);
   static constexpr __int128 PAYLOAD =
       static_cast<__int128>(MakeU128<2, CS...>());
 };
@@ -132,14 +135,23 @@ struct Static128<'0', 'B', CS...> {
 template <char... CS>
 struct Static128<'0', CS...> {
   static constexpr bool IS_VALID =
-      ValidateU128<8, CS...>() && (MakeU128<8, CS...>() < (1_u128 << 127));
+      ValidateU128<8, CS...>() && (MakeU128<8, CS...>() <= SIGNED_LIMIT);
   static constexpr __int128 PAYLOAD =
       static_cast<__int128>(MakeU128<8, CS...>());
 };
 
+}  // namespace suffix128
+
 template <char... CS>
-constexpr __int128 operator""_128() {
-  static_assert(Static128<CS...>::IS_VALID,
+constexpr unsigned __int128 operator"" _u128() {
+  static_assert(suffix128::StaticU128<CS...>::IS_VALID,
                 "Invalid characters or number too large");
-  return Static128<CS...>::PAYLOAD;
+  return suffix128::StaticU128<CS...>::PAYLOAD;
+}
+
+template <char... CS>
+constexpr __int128 operator"" _128() {
+  static_assert(suffix128::Static128<CS...>::IS_VALID,
+                "Invalid characters or number too large");
+  return suffix128::Static128<CS...>::PAYLOAD;
 }
--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#clang-3.8 -stdlib=libc++
+# To run: ./test.sh compiler1 compiler2 compiler3 ...
 
 # TODO: distinguish errors from warnings?
 
@@ -16,12 +16,16 @@ function Expect() {
     shift; TYPE=$1
     shift; DIGITS=$1
     shift; SUFFIX=$1
-    OUTPUT=$(mktemp)
+    OUTPUT="$(mktemp)"
+    if [[ "${STANDARD}" -lt 14 ]]; then
+        DIGITS="$(echo ${DIGITS} | tr -d "'")"
+    fi
     ${CXX} -c -std=c++${STANDARD} -Werror -W -Wall -Wextra -DTYPE="${TYPE}" \
            -DDIGITS=${DIGITS} -DSUFFIX=${SUFFIX} test.cpp &>"${OUTPUT}"
     ACTUAL=$?
     if AsExpected ${EXPECTED} ${ACTUAL}; then
         printf "TESTING%20s: %s\n" "${NAME}" OK
+        rm "${OUTPUT}"
     else
         IO="$(cat ${OUTPUT})"
         rm "${OUTPUT}"
@@ -30,84 +34,101 @@ function Expect() {
     fi
 }
 
-Expect uHex PASS "clang-3.8 -stdlib=libc++" 14 "unsigned __int128" 0x12 u128
-Expect uDecimal PASS "clang-3.8 -stdlib=libc++" 14 "unsigned __int128" 12 u128
-Expect Hex PASS "clang-3.8 -stdlib=libc++" 14 __int128 0x12 128
-Expect Decimal PASS "clang-3.8 -stdlib=libc++" 14 __int128 12 128
+function SmokeTestCompiler {
+    CXX="$1"
+    STANDARD="$2"
+    EMPTY="$(mktemp)"
+    if ! "${CXX}" -c -std=c++${STANDARD} "${EMPTY}" &>/dev/null; then
+        rm "${EMPTY}"
+        return 1
+    fi
+}
 
-Expect uHexOverflow FAIL "clang-3.8 -stdlib=libc++" 14 "unsigned __int128" \
-       "0x1'0000'0000'0000'0000'0000'0000'0000'0000" u128
-Expect uHexNoOverflow PASS "clang-3.8 -stdlib=libc++" 14 "unsigned __int128" \
-       "0xffff'ffff'ffff'ffff'ffff'ffff'ffff'ffff" u128
-Expect uBinOverflow FAIL "clang-3.8 -stdlib=libc++" 14 "unsigned __int128" \
-       "0b1'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000" u128
-Expect uBinNoOverflow PASS "clang-3.8 -stdlib=libc++" 14 "unsigned __int128" \
-       "0b1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111" u128
-Expect uOctOverflow FAIL "clang-3.8 -stdlib=libc++" 14 "unsigned __int128" \
-       "0400'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000" u128
-Expect uOctNoOverflow PASS "clang-3.8 -stdlib=libc++" 14 "unsigned __int128" \
-       "0377'7777'7777'7777'7777'7777'7777'7777'7777'7777'7777" u128
-Expect uDecOverflow FAIL "clang-3.8 -stdlib=libc++" 14 "unsigned __int128" \
-       "340282366920938463463374607431768211456" u128
-Expect uDecNoOverflow PASS "clang-3.8 -stdlib=libc++" 14 "unsigned __int128" \
-       "340282366920938463463374607431768211455" u128
+for CXX in "$@"; do
+    for STANDARD in 11 14; do
+        echo "${CXX} ${STANDARD}"
+        if SmokeTestCompiler "${CXX}" ${STANDARD}; then
+            Expect uHex PASS "${CXX}" "${STANDARD}" "unsigned __int128" 0x12 u128
+            Expect uDecimal PASS "${CXX}" "${STANDARD}" "unsigned __int128" 12 u128
+            Expect Hex PASS "${CXX}" "${STANDARD}" __int128 0x12 128
+            Expect Decimal PASS "${CXX}" "${STANDARD}" __int128 12 128
 
-Expect HexOverflow FAIL "clang-3.8 -stdlib=libc++" 14 __int128 \
-       "0x8000'0000'0000'0000'0000'0000'0000'0000" 128
-Expect HexNoOverflow PASS "clang-3.8 -stdlib=libc++" 14 __int128 \
-       "0x0fff'ffff'ffff'ffff'ffff'ffff'ffff'ffff" 128
-Expect BinOverflow FAIL "clang-3.8 -stdlib=libc++" 14 __int128 \
-       "0b1000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000" 128
-Expect BinNoOverflow PASS "clang-3.8 -stdlib=libc++" 14 __int128 \
-       "0b0111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111" 128
-Expect OctOverflow FAIL "clang-3.8 -stdlib=libc++" 14 __int128 \
-       "0200'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000" 128
-Expect OctNoOverflow PASS "clang-3.8 -stdlib=libc++" 14 __int128 \
-       "0177'7777'7777'7777'7777'7777'7777'7777'7777'7777'7777" 128
-Expect DecOverflow FAIL "clang-3.8 -stdlib=libc++" 14 __int128 \
-       "170141183460469231731687303715884105728" 128
-Expect DecNoOverflow PASS "clang-3.8 -stdlib=libc++" 14 __int128 \
-       "170141183460469231731687303715884105727" 128
+            Expect uHexOverflow FAIL "${CXX}" "${STANDARD}" "unsigned __int128" \
+                   "0x1'0000'0000'0000'0000'0000'0000'0000'0000" u128
+            Expect uHexNoOverflow PASS "${CXX}" "${STANDARD}" "unsigned __int128" \
+                   "0xffff'ffff'ffff'ffff'ffff'ffff'ffff'ffff" u128
+            Expect uBinOverflow FAIL "${CXX}" "${STANDARD}" "unsigned __int128" \
+                   "0b1'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000" u128
+            Expect uBinNoOverflow PASS "${CXX}" "${STANDARD}" "unsigned __int128" \
+                   "0b1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111" u128
+            Expect uOctOverflow FAIL "${CXX}" "${STANDARD}" "unsigned __int128" \
+                   "0400'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000" u128
+            Expect uOctNoOverflow PASS "${CXX}" "${STANDARD}" "unsigned __int128" \
+                   "0377'7777'7777'7777'7777'7777'7777'7777'7777'7777'7777" u128
+            Expect uDecOverflow FAIL "${CXX}" "${STANDARD}" "unsigned __int128" \
+                   "340282366920938463463374607431768211456" u128
+            Expect uDecNoOverflow PASS "${CXX}" "${STANDARD}" "unsigned __int128" \
+                   "340282366920938463463374607431768211455" u128
 
-Expect NegHexOverflow FAIL "clang-3.8 -stdlib=libc++" 14 __int128 \
-       "-0x8000'0000'0000'0000'0000'0000'0000'0000" 128
-Expect NegHexNoOverflow PASS "clang-3.8 -stdlib=libc++" 14 __int128 \
-       "-0x0fff'ffff'ffff'ffff'ffff'ffff'ffff'ffff" 128
-Expect NegBinOverflow FAIL "clang-3.8 -stdlib=libc++" 14 __int128 \
-       "-0b1000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000" 128
-Expect NegBinNoOverflow PASS "clang-3.8 -stdlib=libc++" 14 __int128 \
-       "-0b0111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111" 128
-Expect NegOctOverflow FAIL "clang-3.8 -stdlib=libc++" 14 __int128 \
-       "-0200'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000" 128
-Expect NegOctNoOverflow PASS "clang-3.8 -stdlib=libc++" 14 __int128 \
-       "-0177'7777'7777'7777'7777'7777'7777'7777'7777'7777'7777" 128
-Expect NegDecOverflow FAIL "clang-3.8 -stdlib=libc++" 14 __int128 \
-       "-170141183460469231731687303715884105728" 128
-Expect NegDecNoOverflow PASS "clang-3.8 -stdlib=libc++" 14 __int128 \
-       "-170141183460469231731687303715884105727" 128
+            Expect HexOverflow FAIL "${CXX}" "${STANDARD}" __int128 \
+                   "0x8000'0000'0000'0000'0000'0000'0000'0000" 128
+            Expect HexNoOverflow PASS "${CXX}" "${STANDARD}" __int128 \
+                   "0x0fff'ffff'ffff'ffff'ffff'ffff'ffff'ffff" 128
+            Expect BinOverflow FAIL "${CXX}" "${STANDARD}" __int128 \
+                   "0b1000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000" 128
+            Expect BinNoOverflow PASS "${CXX}" "${STANDARD}" __int128 \
+                   "0b0111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111" 128
+            Expect OctOverflow FAIL "${CXX}" "${STANDARD}" __int128 \
+                   "0200'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000" 128
+            Expect OctNoOverflow PASS "${CXX}" "${STANDARD}" __int128 \
+                   "0177'7777'7777'7777'7777'7777'7777'7777'7777'7777'7777" 128
+            Expect DecOverflow FAIL "${CXX}" "${STANDARD}" __int128 \
+                   "170141183460469231731687303715884105728" 128
+            Expect DecNoOverflow PASS "${CXX}" "${STANDARD}" __int128 \
+                   "170141183460469231731687303715884105727" 128
 
-Expect HexLowerLower PASS "clang-3.8 -stdlib=libc++" 14 "unsigned __int128" \
-       "0xf" u128
-Expect HexUpperLower PASS "clang-3.8 -stdlib=libc++" 14 "unsigned __int128" \
-       "0Xf" u128
-Expect HexLowerUpper PASS "clang-3.8 -stdlib=libc++" 14 "unsigned __int128" \
-       "0xF" u128
-Expect HexUpperUpper PASS "clang-3.8 -stdlib=libc++" 14 "unsigned __int128" \
-       "0XF" u128
-Expect BinLower PASS "clang-3.8 -stdlib=libc++" 14 "unsigned __int128" \
-       "0b1" u128
-Expect BinUpper PASS "clang-3.8 -stdlib=libc++" 14 "unsigned __int128" \
-       "0B1" u128
+            Expect NegHexOverflow FAIL "${CXX}" "${STANDARD}" __int128 \
+                   "-0x8000'0000'0000'0000'0000'0000'0000'0000" 128
+            Expect NegHexNoOverflow PASS "${CXX}" "${STANDARD}" __int128 \
+                   "-0x0fff'ffff'ffff'ffff'ffff'ffff'ffff'ffff" 128
+            Expect NegBinOverflow FAIL "${CXX}" "${STANDARD}" __int128 \
+                   "-0b1000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000" 128
+            Expect NegBinNoOverflow PASS "${CXX}" "${STANDARD}" __int128 \
+                   "-0b0111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111'1111" 128
+            Expect NegOctOverflow FAIL "${CXX}" "${STANDARD}" __int128 \
+                   "-0200'0000'0000'0000'0000'0000'0000'0000'0000'0000'0000" 128
+            Expect NegOctNoOverflow PASS "${CXX}" "${STANDARD}" __int128 \
+                   "-0177'7777'7777'7777'7777'7777'7777'7777'7777'7777'7777" 128
+            Expect NegDecOverflow FAIL "${CXX}" "${STANDARD}" __int128 \
+                   "-170141183460469231731687303715884105728" 128
+            Expect NegDecNoOverflow PASS "${CXX}" "${STANDARD}" __int128 \
+                   "-170141183460469231731687303715884105727" 128
 
-Expect InvalidBase FAIL "clang-3.8 -stdlib=libc++" 14 "unsigned __int128" \
-       "0zF" u128
-Expect MissingDigits FAIL "clang-3.8 -stdlib=libc++" 14 "unsigned __int128" \
-       "0x" u128
-Expect InvalidOctalx FAIL "clang-3.8 -stdlib=libc++" 14 "unsigned __int128" \
-       "00x0" u128
-Expect InvalidHexG FAIL "clang-3.8 -stdlib=libc++" 14 "unsigned __int128" \
-       "0xg" u128
-Expect InvalidOctal8 FAIL "clang-3.8 -stdlib=libc++" 14 "unsigned __int128" \
-       "0080" u128
-Expect InvalidBin2 FAIL "clang-3.8 -stdlib=libc++" 14 "unsigned __int128" \
-       "0b2" u128
+            Expect HexLowerLower PASS "${CXX}" "${STANDARD}" "unsigned __int128" \
+                   "0xf" u128
+            Expect HexUpperLower PASS "${CXX}" "${STANDARD}" "unsigned __int128" \
+                   "0Xf" u128
+            Expect HexLowerUpper PASS "${CXX}" "${STANDARD}" "unsigned __int128" \
+                   "0xF" u128
+            Expect HexUpperUpper PASS "${CXX}" "${STANDARD}" "unsigned __int128" \
+                   "0XF" u128
+            Expect BinLower PASS "${CXX}" "${STANDARD}" "unsigned __int128" \
+                   "0b1" u128
+            Expect BinUpper PASS "${CXX}" "${STANDARD}" "unsigned __int128" \
+                   "0B1" u128
+
+            Expect InvalidBase FAIL "${CXX}" "${STANDARD}" "unsigned __int128" \
+                   "0zF" u128
+            Expect MissingDigits FAIL "${CXX}" "${STANDARD}" "unsigned __int128" \
+                   "0x" u128
+            Expect InvalidOctalx FAIL "${CXX}" "${STANDARD}" "unsigned __int128" \
+                   "00x0" u128
+            Expect InvalidHexG FAIL "${CXX}" "${STANDARD}" "unsigned __int128" \
+                   "0xg" u128
+            Expect InvalidOctal8 FAIL "${CXX}" "${STANDARD}" "unsigned __int128" \
+                   "0080" u128
+            Expect InvalidBin2 FAIL "${CXX}" "${STANDARD}" "unsigned __int128" \
+                   "0b2" u128
+        fi
+    done
+done
